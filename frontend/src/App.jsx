c9e5d2888ce658efb04032cc66bf4ba8a041530e
Ash-art-arch <ashishsaikia092@gmail.com>
2025-07-01 11:52:18 +0530

    Removed Unnecessary Files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,12 @@ import { useState } from 'react'
 import './App.css'
 import {Routes,Route} from "react-router-dom"
 import LandingPage from './pages/LandingPage'
+
 import SignUpUser from './components/SignupUser'
 import SignUpSeller from './components/SignupSeller'
 import Login from './components/Login'
+
+import Cart from './pages/Cart'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -14,7 +17,12 @@ function App() {
       <Route path='/login' element={<Login/>} />
       <Route path='/signupSeller' element={<SignUpSeller/>} />
       <Route path='/signupUser' element={<SignUpUser/>} />
-      <Route path='/' element={<LandingPage/>} />
+      
+
+      <Route path='/' element={<LandingPage/>}/>
+      <Route path='/Cart' element={<Cart/>}/>
+      
+
      </Routes>
     </>
   )
