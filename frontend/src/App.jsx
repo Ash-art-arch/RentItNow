8dcb9295e08641f8ea29570a0188c9a5db71769f
Ash-art-arch <ashishsaikia092@gmail.com>
2025-07-01 17:00:57 +0530

    fixed conflict s

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,12 +11,6 @@ import Cart from './pages/Cart'
 import PaymentPage from './pages/PaymentPage'
 import ProductPage from './pages/ProductPage'
 import CategoriesPAge from './pages/CategoriesPAge'
-<<<<<<< HEAD
-=======
-import PaymentPage from './pages/PaymentPage'
-import ProductPage from './pages/ProductPage'
-
->>>>>>> sampriti
 
 function App() {
   const [count, setCount] = useState(0)
