8779d7718b984ec7ab7656cfcbc24de6b06b24a0
Ash-art-arch <ashishsaikia092@gmail.com>
2025-07-01 16:47:41 +0530

    Added Categories With Backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,10 @@ const categoryRouter = require("./routes/categories.route");
 
 dotenv.config();
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 app.use(cookieParser())
 app.use(express.json())
 
