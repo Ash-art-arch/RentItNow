f7a60ff2712c91fb5b29aa521b85bf517c797a67
Nabadeep Dutta <nabadeepdutta8@gmail.com>
2025-07-10 12:05:21 +0530

    Order history page

--- frontend/src/pages/ProductEdit.jsx
+++ frontend/src/pages/ProductEdit.jsx
@@ -1,7 +1,7 @@
 
 import DashboardHeader from  "../components/DashboardHeader"
 import Footer from "../components/Footer";
-import Editproduct from "../components/editproduct";
+import Editproduct from "../components/Editproduct";
 const ProductEdit = () => {
   return (
     <div>
