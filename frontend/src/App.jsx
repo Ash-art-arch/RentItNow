25f9fab17da49a3fd908b7e2033e475b490d08c1
Ash-art-arch <ashishsaikia092@gmail.com>
2025-07-11 12:17:24 +0530

    Responsive

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,31 @@
-import { useContext, useEffect, useState } from 'react';
-import './App.css';
+import { useContext, useEffect, useState } from "react";
+import "./App.css";
 import { Routes, Route, Navigate } from "react-router-dom";
-import LandingPage from './pages/LandingPage';
-import SignUpUser from './components/SignupUser';
-import Login from './components/Login';
-import Cart from './pages/Cart';
-import PaymentPage from './pages/PaymentPage';
-import ProductPage from './pages/ProductPage';
-import CategoriesPage from './pages/CategoriesPAge';
-import Dashboard from './pages/Dashboard.jsx';
-import ProductUpload from'./pages/ProductUpload.jsx';
-import CustomerResponse from './pages/CustomerResponse.jsx'
-import Settings from './pages/Settings.jsx'
-import ProductEdit from './pages/ProductEdit.jsx';
-import Loader from'./components/Loader.jsx';
-import { userContext } from './providers/userProviders';
-import { useDispatch } from 'react-redux';
-import { addToCart } from './Features/cartReducer';
-import { loadCartFromBackend } from './utils/loadCart';
-import Success from './pages/SuccessPage.jsx';
+import LandingPage from "./pages/LandingPage";
+import SignUpUser from "./components/SignupUser";
+import Login from "./components/Login";
+import Cart from "./pages/Cart";
+import PaymentPage from "./pages/PaymentPage";
+import ProductPage from "./pages/ProductPage";
+import CategoriesPage from "./pages/CategoriesPAge";
+import Dashboard from "./pages/Dashboard.jsx";
+import ProductUpload from "./pages/ProductUpload.jsx";
+import CustomerResponse from "./pages/CustomerResponse.jsx";
+import Settings from "./pages/Settings.jsx";
+import ProductEdit from "./pages/ProductEdit.jsx";
+import Loader from "./components/Loader.jsx";
+import { userContext } from "./providers/userProviders";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart, clearCart } from "./Features/cartReducer";
+import { loadCartFromBackend } from "./utils/loadCart";
+import Success from "./pages/SuccessPage.jsx";
 
 function App() {
   const { user, setUser } = useContext(userContext);
   const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
+  const cartItem = useSelector((state) => state.cart.items);
 
-  
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
     if (storedUser) {
@@ -33,7 +33,6 @@ function App() {
     }
   }, [setUser]);
 
- 
   useEffect(() => {
     const fetchProfile = async () => {
       try {
@@ -41,12 +40,11 @@ function App() {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
-            
           },
-          credentials:"include"
+          credentials: "include",
         });
 
-         if (!res.ok) {
+        if (!res.ok) {
           console.log("Token invalid or expired. Logging out.");
           localStorage.removeItem("token");
           localStorage.removeItem("user");
@@ -63,39 +61,39 @@ function App() {
         setLoading(false);
       }
     };
-  
+
     fetchProfile();
   }, [setUser]);
- 
 
- 
   useEffect(() => {
     const loadUserCart = async () => {
       console.log("user", user);
-      if (!user || !user._id) return;
+      if (!user || !user.id) return;
 
       try {
-        const cartData = await loadCartFromBackend(user._id);
+        const cartData = await loadCartFromBackend(user.id);
         console.log("cartdata", cartData);
         if (cartData?.length) {
-          cartData.forEach(item => {
-            if (item && item.item)  {
-    console.log("🧩 Dispatching addToCart for item:", {
-      id: item.item._id,
-      title: item.item.name,
-      quantity: item.quantity
-    });
-            
-              dispatch(addToCart({
-                id: item.item._id || item.item,
-                title: item.item.name || "Product",
-                size: "Default Size",
-                color: "Default Color",
-                price: item.item.price || 0,
-                image: item.item.images?.[0] || "",
-                quantity: item.quantity || 1
-              
-              }));
+          dispatch(clearCart())
+          cartData.forEach((item) => {
+            if (item && item.item) {
+              console.log("🧩 Dispatching addToCart for item:", {
+                id: item.item._id,
+                title: item.item.name,
+                quantity: item.quantity,
+              });
+
+              dispatch(
+                addToCart({
+                  id: item.item._id || item.item,
+                  title: item.item.name || "Product",
+                  size: "Default Size",
+                  color: "Default Color",
+                  price: item.item.price || 0,
+                  image: item.item.images?.[0] || "",
+                  quantity: item.quantity || 1,
+                })
+              );
             }
           });
         }
@@ -103,38 +101,63 @@ function App() {
         console.error("Failed to load cart on app start:", err.message);
       }
     };
-
-    loadUserCart();
+    if (cartItem.length === 0) {
+      loadUserCart();
+    }
   }, [dispatch, user]);
 
-  if(loading){
-    return(
-     <Loader/>
-    )
+  if (loading) {
+    return <Loader />;
   }
-  
+
   return (
     <>
       <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/signup' element={<SignUpUser />} />
-        <Route path='/' element={<LandingPage />} />
-        <Route path='/cart' element={<Cart />} />
-        <Route path='/cart/payment' element={<PaymentPage />} />
-        <Route path='/productpage' element={<ProductPage />} />
-        <Route path='/categories' element={<CategoriesPage />} />
-          <Route path = '/productedit'element={<ProductEdit/>}/>
-           <Route path='/productedit/productupload' element={<ProductUpload />} />
-           <Route path="/productupload/:id" element={<ProductUpload />} />
-        <Route path='/dashboard' element={user?.role=="Seller"?<Dashboard />:<Navigate to={'/signup?role=Seller'}/>} />
-        <Route path='/productupload' element={user?.role=="Seller"?<ProductUpload />:<Navigate to={'/signup?role=Seller'}/>} />
-        <Route path='/customerresponse' element={user?.role=="Seller"?<CustomerResponse/>:<Navigate to={'/signup?role=Seller'}/>} />
-        <Route path='/settings' element={<Settings/>}/>
-        <Route path='/Success' element={<Success/>}/>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUpUser />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/cart/payment" element={<PaymentPage />} />
+        <Route path="/productpage" element={<ProductPage />} />
+        <Route path="/categories" element={<CategoriesPage />} />
+        <Route path="/productedit" element={<ProductEdit />} />
+        <Route path="/productedit/productupload" element={<ProductUpload />} />
+        <Route path="/productupload/:id" element={<ProductUpload />} />
+        <Route
+          path="/dashboard"
+          element={
+            user?.role == "Seller" ? (
+              <Dashboard />
+            ) : (
+              <Navigate to={"/signup?role=Seller"} />
+            )
+          }
+        />
+        <Route
+          path="/productupload"
+          element={
+            user?.role == "Seller" ? (
+              <ProductUpload />
+            ) : (
+              <Navigate to={"/signup?role=Seller"} />
+            )
+          }
+        />
+        <Route
+          path="/customerresponse"
+          element={
+            user?.role == "Seller" ? (
+              <CustomerResponse />
+            ) : (
+              <Navigate to={"/signup?role=Seller"} />
+            )
+          }
+        />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/Success" element={<Success />} />
       </Routes>
     </>
   );
 }
 
 export default App;
-
--- frontend/src/components/CartComp.jsx
+++ frontend/src/components/CartComp.jsx
@@ -1,81 +1,72 @@
-import React, { useContext } from "react";
-import { useEffect } from "react"
+import React, { useContext, useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addToCart, increaseQty, decreaseQty, removeItem ,clearCart} from "../Features/cartReducer";
-import {syncCartToBackend} from "../utils/syncCart";
-import {loadCartFromBackend} from "../utils/loadCart";
+import {
+  addToCart,
+  increaseQty,
+  decreaseQty,
+  removeItem,
+  clearCart,
+} from "../Features/cartReducer";
+import { syncCartToBackend } from "../utils/syncCart";
 import { userContext } from "../providers/userProviders";
 import { useNavigate } from "react-router-dom";
-const  CartComp = () => {
+
+const CartComp = () => {
   const dispatch = useDispatch();
-  
- const cartItems = useSelector((state) => state.cart.items);
-const totalPrice = useSelector((state) => state.cart.totalPrice);
-  const {user} = useContext(userContext)
-useEffect(() => {
-  console.log(" Redux cart items in CartComp:",cartItems);
-}, [cartItems]);
+  const navigate = useNavigate();
+  const { user } = useContext(userContext);
 
-const userId = user.id
-console.log(userId)
-console.log("UserID",userId)
+  const cartItems = useSelector((state) => state.cart.items);
+  const totalPrice = useSelector((state) => state.cart.totalPrice);
+  const userId = user.id;
+
+  const [loadingCart, setLoadingCart] = useState(false);
+
+  // Sync cart to backend whenever cartItems change
   useEffect(() => {
-  const fetchCart = async () => {
-    if (userId) {
-          
-      try {
-        const backendCart = await loadCartFromBackend(userId);
-        
-console.log("Cart fetched from backend:", backendCart);
-dispatch(clearCart())
-        backendCart.forEach(item => {
-          if (item) {
-            dispatch(addToCart({
-              id: item.item._id || item.item,
-              title: item.item.name || "Product",
-              size: "Default Size",
-              color: "Default Color",
-              price: item.item.price || 0,
-              image: item.item.images?.[0] || "",
-              quantity: item.quantity || 1
-            }));
-            console.log(
-              "Item Added"
-            )
-          }
-        });
-        
-      } catch (e) {
-        console.error("Cart loading failed:", e.message);
-      }
-    }
-  };
-  fetchCart();
-}, [userId, dispatch]);
-console.log("Cart Items=>",cartItems)
-useEffect(() => {
-    if (userId && cartItems.length > 0) {
-      console.log("Reached Here")
-
-      const formattedCart  = cartItems.map((item) => ({
-        item: item.id ,
+    if (userId && cartItems.length>=0&& !loadingCart) {
+      const formattedCart = cartItems.map((item) => ({
+        item: item.id,
         quantity: item.quantity,
+        startDate: item.startDate || "",
+        endDate: item.endDate || "",
       }));
-      console.log("sync to backend", formattedCart);
+      console.log("Syncing to backend:", formattedCart);
       syncCartToBackend(userId, formattedCart);
-      
     }
-  }, [cartItems, userId]);
-  const navigate = useNavigate()
-  const handleCheckout=()=>{
-        navigate('/cart/payment')
-  }
+  }, [cartItems, userId, loadingCart]);
+
+  const handleCheckout = () => {
+    if (cartItems.length === 0) {
+      alert("Your cart is empty.");
+      return;
+    }
+    const { startDate, endDate } = cartItems[0];
+    if (!startDate || !endDate) {
+      alert("Start and End dates are missing.");
+      return;
+    }
+    navigate("/cart/payment", { state: { startDate, endDate } });
+  };
+
+  const handleRemove = (itemId) => {
+    dispatch(removeItem(itemId));
+    const updatedCart = cartItems
+      .filter((i) => i.id !== itemId)
+      .map((i) => ({
+        item: i.id,
+        quantity: i.quantity,
+        startDate: i.startDate || "",
+        endDate: i.endDate || "",
+      }));
+    syncCartToBackend(userId, updatedCart);
+  };
+
   return (
-    <div className="min-h-screen  p-8" style={{ backgroundColor: "rgba(0, 0, 0, 0.85)"}}>
-      <div className="grid grid-cols-[3fr_1.3fr]  gap-8 bg-white rounded-md shadow p-6 mt-20 ">
-        
+    <div className="min-h-screen p-4 sm:p-8" style={{ backgroundColor: "rgba(0, 0, 0, 0.85)" }}>
+      <div className="grid grid-cols-1 lg:grid-cols-[3fr_1.3fr] gap-8 bg-white rounded-md shadow p-4 sm:p-6 mt-20">
         <div>
-          <div className="grid grid-cols-[270px_1.5fr_1fr_1fr_40px] items-center text-gray-500 font-semibold text-sm border-b pb-4 mb-4">
+          <div className="hidden md:grid grid-cols-[270px_1.5fr_1fr_1fr_40px] items-center text-gray-500 font-semibold text-sm border-b pb-4 mb-4">
             <h3>Image</h3>
             <h3>Product</h3>
             <h3>Quantity</h3>
@@ -83,10 +74,10 @@ useEffect(() => {
             <h3>Remove</h3>
           </div>
 
-          {cartItems&&cartItems.map((item) => (
+          {cartItems.map((item) => (
             <div
               key={item.id}
-              className="grid grid-cols-[200px_1.5fr_1fr_1fr_40px] items-center gap-10 py-4 border-b "
+              className="grid grid-cols-1 md:grid-cols-[200px_1.5fr_1fr_1fr_40px] items-center gap-4 md:gap-10 py-4 border-b"
             >
               <img
                 src={item.image}
@@ -101,32 +92,48 @@ useEffect(() => {
               </div>
 
               <div className="flex items-center gap-2">
-                <button className="w-8 h-8 bg-[#FFE500] text-black rounded-full text-sm font-bold flex items-center justify-center"onClick={() => dispatch(increaseQty(item.id))}
->+</button>
-                <span className="border px-4 py-[3px] rounded text-center">{item.quantity}</span>
-                <button className="w-8 h-8 bg-black text-white rounded-full text-sm font-bold flex items-center justify-center" onClick={() => dispatch(decreaseQty(item.id))}
->-</button>
+                <button
+                  className="w-8 h-8 bg-[#FFE500] text-black rounded-full text-sm font-bold flex items-center justify-center"
+                  onClick={() => dispatch(increaseQty(item.id))}
+                >
+                  +
+                </button>
+                <span className="border px-4 py-[3px] rounded text-center">
+                  {item.quantity}
+                </span>
+                <button
+                  className="w-8 h-8 bg-black text-white rounded-full text-sm font-bold flex items-center justify-center"
+                  onClick={() => dispatch(decreaseQty(item.id))}
+                >
+                  -
+                </button>
               </div>
 
-              <div className="font-medium">${item.price.toFixed(2)}</div>
+              <div className="font-medium">
+                ₹{(item.price * (item.quantity || 1)).toFixed(2)}
+              </div>
 
-              <button className="text-red-500 text-xl hover:underline"  onClick={() => dispatch(removeItem(item.id))}>×</button>
+              <button
+                className="text-red-500 text-xl hover:underline"
+                onClick={() => handleRemove(item.id)}
+              >
+                ×
+              </button>
             </div>
           ))}
         </div>
 
-        
         <div className="bg-gray-50 p-6 rounded shadow-inner h-fit">
           <h2 className="text-lg font-semibold mb-4">SUMMARY</h2>
 
           <div className="text-sm text-gray-700 mb-2 space-y-1">
             <div className="flex justify-between">
-              <span>Subtotal : </span>
-              <span>{totalPrice}</span>
+              <span>Subtotal :</span>
+              <span>₹{totalPrice.toFixed(2)}</span>
             </div>
             <div className="flex justify-between">
               <span>Shipping</span>
-              <span>$5.00</span>
+              <span>₹5.00</span>
             </div>
           </div>
 
@@ -134,10 +141,13 @@ useEffect(() => {
 
           <div className="flex justify-between text-lg font-bold mb-4">
             <span>Total</span>
-            <span>{totalPrice + 5}</span>
+            <span>₹{(totalPrice + 5).toFixed(2)}</span>
           </div>
 
-          <button className="w-full bg-black text-white py-2 rounded hover:bg-gray-900 mb-2" onClick={handleCheckout}>
+          <button
+            className="w-full bg-black text-white py-2 rounded hover:bg-gray-900 mb-2"
+            onClick={handleCheckout}
+          >
             CHECK OUT
           </button>
 
@@ -146,12 +156,14 @@ useEffect(() => {
           </button>
 
           <p className="text-xs text-gray-500 mt-4 leading-5">
-            <strong>FREE SHIPPING</strong><br />
+            <strong>FREE SHIPPING</strong>
+            <br />
             Your order qualifies for free shipping.
           </p>
 
           <p className="text-xs text-gray-500 mt-4">
-            <strong>NEED HELP?</strong><br />
+            <strong>NEED HELP?</strong>
+            <br />
             Contact our support team.
           </p>
         </div>
